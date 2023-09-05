4e4d2064ace1dcc4736435cd6134367a16c87dd2
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-09-05 11:39:34 +0530

    Added solution - LeetHub

---
+++ Pattern - GFG/pattern.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printDiamond(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                cout<<" ";
+            }
+            for(int j=0;j<1*i+1;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<i;j++){
+                cout<<" ";
+            }
+            for(int j=0;j<n-i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printDiamond(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
